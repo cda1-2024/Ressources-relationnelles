9d59988f4d57504889e441d5b79aa15519dfc6a9
EstebanRacine <racine.esteban@lyceepergaud.fr>
2025-03-13 11:55:18 +0100

    FRONTEND - Ajout d'Angular Material

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'frontend';
 }
