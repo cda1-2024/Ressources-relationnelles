9e45c4b64206db29b6f630804cc3b0fd8114935e
EstebanRacine <esteban.racineecole@gmail.com>
2025-05-13 11:53:01 +0200

    Page d'accueil + Assets + NavBar

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,59 @@
+import { Component, ViewChild } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../components/modal/login_modal/login_modal.component';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BreakpointService } from '../services/breackpoint.service';
+import { NgIf } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from './../auth/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+    MatSidenavModule,
+    NgIf
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+  isMobile: boolean = false;
+  isLoggedIn: boolean = false;
+  
+    constructor(private breakpointService: BreakpointService, private authService: AuthService , private dialog: MatDialog) {
+      this.isMobile = this.breakpointService.isMobile();
+      this.breakpointService.isMobile$.subscribe((isMobile) => {
+      this.isMobile = isMobile;
+
+      this.isLoggedIn = this.authService.isLoggedIn();
+      this.authService.isLoggedIn$.subscribe((loggedIn) => {
+        this.isLoggedIn = loggedIn;  // Mise à jour de l'état de la connexion
+      });
+      });
+  }
+
+  openLoginDialog(): void {
+    console.log('Login button clicked');
+    this.dialog.open(LoginComponent, {
+      width: '400px'
+    });
+  }
+
+  onLogout(): void {
+    console.log('Logout button clicked');
+    this.authService.logout();  // Appelle la méthode de déconnexion
+  }
+
+  
+
+}
