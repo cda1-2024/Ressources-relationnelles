b2e0f487ba68e59bbb041c43ba25edae8777919f
Sneaxiii <misterbalise2@gmail.com>
2025-09-02 01:20:27 +0200

    Break dev setup

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://backend:3000/api',
-  urlMedia: 'http://cacaboudin',
+  apiUrl: 'http://92.112.192.100:3000/api',
+  urlMedia: 'http://92.112.192.100:3000',
 };
